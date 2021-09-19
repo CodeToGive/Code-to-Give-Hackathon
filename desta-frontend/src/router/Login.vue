51d3611074e4d3cdf8643f7a37045621fd874199
ben12mwaniki <ben12mwaniki@gmail.com>
2021-09-18 23:18:13 -0400

    added javascript for accessing backend

--- desta-frontend/src/router/Login.vue
+++ desta-frontend/src/router/Login.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col -mt-32 bg-white px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-xl shadow-2xl w-full max-w-md  border-l-4 border-blue-900">
         <div class="font-medium self-center text-xl sm:text-2xl uppercase w-30 text-center text-blue-900 rounded-full p-6 ">Sign In</div>
         <div class="mt-10">
-            <form method="POST" @submit="onSubmit" action="" autocomplete="">
+            <form action="" autocomplete="">
                 <div class="relative w-full mb-3">
                     <input v-model="email" type="email" name="email" class="border-2 p-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm  focus:outline-none focus:ring w-full" placeholder="Email" style="transition: all 0.15s ease 0s;" />
                     <small class="p-2 text-red-500">* Email</small>
@@ -13,7 +13,7 @@
                     <small class="p-2 text-red-500">* Password</small>
                 </div>
                 <div class="text-center mt-6">
-                    <input type="submit" name="signin" id="signin" value="Sign In" class="p-3 rounded-full bg-blue-900 outline-none text-white shadow w-32 justify-center focus:bg-purple-700 hover:bg-purple-500" required>
+                    <input type="button" @click="login(email,password)" name="signin" id="signin" value="Sign In" class="p-3 rounded-full bg-blue-900 outline-none text-white shadow w-32 justify-center focus:bg-purple-700 hover:bg-purple-500" required>
                 </div>
                 <div class="flex flex-wrap mt-6">
                     <div class="w-1/2 text-left">
@@ -34,17 +34,33 @@ export default {
   data() {
     return {
       email: '', // When user inputs the email, it'll be stored here
-      password: ''
+      password: '',
+      errorLog:'',
+      msgLog:""
     }
   },
   methods: {
-    onSubmit(event) {
-      event.preventDefault()
-      // TODO make GET request to the backend to get the data here and open profile page?
-      this.$router.push({path: '/Profile', params: {email}})
-      // this.router.push({name: 'user', params: {id: '12345'}})
-    }
-  },
+    signIn: function(email, password){
+      
+
+if(email==''|| password==""){
+errorLog="Email/Password is empty"
+}else{
+ const details = {'email': email, 'password':password}
+ AXIOS.post('/login', {details}, {}).then(response =>{
+            this.email='', this.password='', this.cpassword='', this.business_name='', this.phone_number='', this.website_link='', this.instagram_link=''
+            this.msgLog="You are logged in!"
+        }).catch(e =>{
+            this.errorLog = e.response.data.message
+        })
+  
+}
+
+
+
+}
+
+  }
 }
 
 
